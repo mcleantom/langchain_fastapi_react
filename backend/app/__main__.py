5ad0159341102b424cc31611fece529155ed81e7
mcleantom <mcleantom97@gmail.com>
2025-04-10 17:12:30 +0100

    Added basic frontend + changed backend api to have coder agent

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -1,52 +1,51 @@
-from fastapi import FastAPI, WebSocket, Depends
-from fastapi.responses import HTMLResponse
-from app.graph import get_graph
-from app.template import html
-from app.memory import get_checkpointer
+from fastapi import FastAPI
 from pydantic import BaseModel
-from langchain_core.messages.base import BaseMessage
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from langchain.schema import AIMessage, HumanMessage, SystemMessage, BaseMessage
+from dotenv import load_dotenv
+from app.graph import graph
+
+
+load_dotenv()
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
-class ChatInput(BaseModel):
-    messages: list[str]
-    thread_id: str
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class ChatRequest(BaseModel):
+    messages: List[BaseMessage]
 
 
 @app.post("/chat")
-async def chat(chat_input: ChatInput, graph=Depends(get_graph)) -> BaseMessage:
-    config = {"configurable": {"thread_id": chat_input.thread_id}}
-    response = await graph.ainvoke({"messages": chat_input.messages}, config=config)
-    return response["messages"][-1]
-
-
-@app.get("/")
-def get():
-    return HTMLResponse(html)
-
-
-@app.websocket("/ws/{thread_id}")
-async def websocket_endpoint(websocket: WebSocket, thread_id: str, graph=Depends(get_graph)):
-    config = {"configurable": {"thread_id": thread_id}}
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        async for event in graph.astream({"messages": [data]}, config=config, stream_mode="messages"):
-            await websocket.send_text(event[0].content)
-
-
-@app.get("/chat/{chat_id}")
-async def get_history(chat_id: str, memory=Depends(get_checkpointer)) -> list[BaseMessage]:
-    messages = await memory.aget({"configurable": {"thread_id": chat_id}})
-    return messages["channel_values"]["messages"]
+async def chat_endpoint(chat_request: ChatRequest):
+    lc_messages = []
+    for msg in chat_request.messages:
+        lc_messages.append(
+            (
+                msg.type,
+                msg.content
+            )
+        )
+    response = []
+    for s in graph.stream(
+            {"messages": lc_messages}, subgraphs=True
+    ):
+        response.append(s)
+    return response
 
 
 if __name__ == "__main__":
-    import sys
-    import asyncio
-    if sys.platform == "win32":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     import uvicorn
     uvicorn.run(app)
