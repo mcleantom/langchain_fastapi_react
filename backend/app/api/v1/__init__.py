08e58a1405ab0335212703ffe748695b1703e080
mcleantom <mcleantom97@gmail.com>
2025-04-15 16:40:15 +0100

    delete auth router, only used for testing

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,9 +1,7 @@
 from fastapi import APIRouter
-from .auth_router import router as auth_router
 from .chat_router import router as chat_router
 from .user_router import router as user_router
 
 router = APIRouter()
-router.include_router(auth_router)
 router.include_router(chat_router)
 router.include_router(user_router)
